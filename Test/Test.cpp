9b0bb5f8fb9ba78cded7aeb8dc00582f6dfdd4e7
dan1lka257 <daniltursunov597@gmail.com>
2025-02-07 13:20:30 +0300

    add cln project

---
+++ Test/Test.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+
+template <typename T>
+void print(T container) {
+	int i = 0;
+	for (auto elem : container) {
+		std::cout << i << ":\t" << elem << '\n';
+		++i;
+	}
+}
+
+int main()
+{
+	std::vector<double> v;
+	v.push_back(0.25);
+	for (int i = 0; i < 100; ++i) {
+		v.push_back((v[i] + 0.1) * 1.06);
+	}
+	print(v);
+}
