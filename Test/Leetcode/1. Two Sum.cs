1385c13ca48897ee802db9d149cf13dd72af0d21
dan1lka257 <daniltursunov597@gmail.com>
2025-02-11 14:11:52 +0300

    solve 10 yandex-leetcode tasks

---
+++ Test/Leetcode/1. Two Sum.cs
@@ -0,0 +1,24 @@
+﻿// Leetcode
+// 1. Two Sum
+
+class Solution
+{
+    public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        vector<int> ans;
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (hash.find(target - nums[i]) != hash.end() && hash[target - nums[i]] != i)
+            {
+                ans.push_back(i);
+                ans.push_back(hash[target - nums[i]]);
+                break;
+            }
+            hash[nums[i]] = i;
+        }
+        return ans;
+
+    }
+};
---
+++ Test/Leetcode/98. Validate Binary Search Tree.cs
@@ -0,0 +1,32 @@
+﻿// Leetcode
+// 98. Validate Binary Search Tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+
+    bool isPossible(TreeNode* root, int64_t l, int64_t r)
+    {
+        if (root == nullptr) return true;
+        if (root->val < r and root->val > l)
+        return isPossible(root->left, l, root->val) && isPossible(root->right, root->val, r);
+    else return false;
+    }
+
+    public:
+    bool isValidBST(TreeNode* root)
+    {
+        int64_t min = -2147483649, max = 2147483648;
+        return isPossible(root, min, max);
+    }
+};
